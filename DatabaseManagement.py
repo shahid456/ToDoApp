fb24a49466e11f9cbf3036e72805ed4feaf1fc09
naumanurrehman <naumanurrehman786@gmail.com>
2019-01-30 11:37:53 +0500

    DatabaseManagement structure defined

--- DatabaseManagement.py
+++ DatabaseManagement.py
@@ -1,3 +1,11 @@
 class DatabaseManagement:
     def __init__(self):
         print('Database constructor called')
+
+    def write(self):
+        print('Insertion done')
+        return 0
+
+    def read(self):
+        print('Read done')
+        return []
