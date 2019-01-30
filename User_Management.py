2df06ce43cce04363c20141df37851101bf7d76b
shahid456 <shahid.jamil@emumba.com>
2019-01-30 12:59:32 +0500

    shahid

--- User_Management.py
+++ User_Management.py
@@ -2,9 +2,15 @@ from User_Management_Model import User_Management_Model as Model
 class User_Man(Model):
 
     def _init_(self):
-        self.model=Model
+        self.model=Model()
     def register(self,user,password):
-        Model.registeruser(user,password)
+        self.model.registeruser(user,password)
+    def login(self):
+        name=input("Enter User name")
+        Pass=input("Enter User's Pass")
+        return self.model.LoginUser(name,Pass)
+    def deleteuser(self,username):
+        pass
 
 
 
--- User_Management_Model.py
+++ User_Management_Model.py
@@ -1,13 +1,33 @@
-from DatabaseManagement import DatabaseManagement as data
+from DatabaseManagement import DatabaseManagement
 import json
+import utilities
 class User_Management_Model:
     def _init_(self):
-        pass
+        self.data=DatabaseManagement('user.json')
+        self.f=self.data.read()
     def registeruser(self,usr,password):
-        f=data.read()
-        if f[usr] in f:
-            print("Already a User")
-        else:
-            d={'userid':' ','username':usr,'password':password}
-            f.update(d)
-            data.write(f)
+        check=0
+        for x in range(len(self.f)):
+            if self.f[x]['username']==usr:
+                check=1
+        if check==0:
+            d={'userid':utilities.generate_unique_id(),'username':usr,'password':password}
+            self.f.append(d)
+            self.data.write(self.f)
+    def LoginUser(self,usr,name):
+        check = -1
+        for x in range(len(self.f)):
+            if self.f[x]['username'] == usr:
+                check = x
+        if  check!=-1:
+            return self.f[x]['userid']
+        return -1
+    def deleteuser(self,usrname):
+        check = 0
+        for x in range(len(self.f)):
+            if self.f[x]['username'] == usrname:
+                self.f.pop(x)
+                break
+
+
+
