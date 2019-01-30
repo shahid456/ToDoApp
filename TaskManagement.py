960a7292ab0ded3d4e146d0c4fe5812f1af6476b
naumanurrehman <naumanurrehman786@gmail.com>
2019-01-30 13:24:45 +0500

    More functionality added

--- TaskManagement.py
+++ TaskManagement.py
@@ -7,15 +7,16 @@ class TaskManagement (TaskManagementMod):
         print("Task management constructed")
 
     def add(self, date, content):
-
+        super().insertTask(date, content)
         print("Add called")
 
     def list(self, options):
         print("List called")
-        return []
+        return self.userTasks
 
     def update(self, id, field, content):
         print('Update Called')
+        super().updateTask(id, field, content)
 
     def delete(self, option, value):
         print('Delete called')
--- TaskManagementMod.py
+++ TaskManagementMod.py
@@ -13,16 +13,10 @@ class TaskManagementMod:
         self.__database = DatabaseManagement(self.filename)
         self.__fetchUserTasks()
 
-    def __fetchUserTasks(self):
-        self.tasks = self.__database.read()
-        for task in self.__tasks:
-            if task['userid'] == self.userId:
-                self.userTasks.append(task)
-
     def insertTask(self, date, content):
-        task_id = utilities.generate_unique_id()
+        taskId = utilities.generate_unique_id()
         task = {
-            'taskid': task_id,
+            'taskid': taskId,
             'userid': self.userId,
             'date': date,
             'content': content
@@ -31,3 +25,35 @@ class TaskManagementMod:
         self.tasks.append(task)
         self.__database.write(self.tasks)
         print("Task inserted")
+
+    def deleteTask(self, taskId):
+        for x in range(len(self.userTasks)):
+            if self.userTasks[x]['taskid'] == taskId:
+                self.userTasks[x].pop()
+                break
+        for x in range(len(self.tasks)):
+            if self.tasks[x]['taskid'] == taskId:
+                self.tasks.pop(x)
+                break
+        self.__database.write(self.tasks)
+
+
+    def updateTask(self, taskId, field, content):
+        for x in range(len(self.userTasks)):
+            if self.userTasks[x]['taskid'] == taskId:
+                self.userTasks[x][field] = content
+                break
+
+        for x in range(len(self.tasks)):
+            if self.tasks[x]['taskid'] == taskId:
+                self.tasks[x][field] = content
+                break
+
+        self.__database.write(self.tasks)
+
+
+    def __fetchUserTasks(self):
+        self.tasks = self.__database.read()
+        for task in self.__tasks:
+            if task['userid'] == self.userId:
+                self.userTasks.append(task)
