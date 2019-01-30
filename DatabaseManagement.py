23a67e67d05b27aeedadfb769eca24506f9d29aa
naumanurrehman <naumanurrehman786@gmail.com>
2019-01-30 14:32:06 +0500

    Database Management is defined

--- DatabaseManagement.py
+++ DatabaseManagement.py
@@ -1,11 +1,23 @@
+import json
+
+
 class DatabaseManagement:
     def __init__(self, filename):
+        self.filename = filename
         print('Database constructor called')
 
     def write(self, content):
         print('Insertion done')
-        return 0
+        if not content:
+            return False
+        jcon = json.dumps(content)
+        with open(self.filename, 'w') as file:
+            file.write(jcon)
 
     def read(self):
-        print('Read done')
-        return []
+        data = []
+        with open(self.filename, 'r') as file:
+            res = str.join('', file.readlines())
+            if str.strip(res) != '':
+                data = json.loads(res)
+        return data
