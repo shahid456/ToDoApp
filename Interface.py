fa7eed9da9d6ab4a633e10a31766be096403b089
naumanurrehman <naumanurrehman786@gmail.com>
2019-01-31 12:08:18 +0500

    list all

--- Interface.py
+++ Interface.py
@@ -45,6 +45,7 @@ class Interface:
                 elif first_part_of_log_command == log_commands_allowed[1]:
                     if len(log_command) == 1:
                         self.TM.list()
+                        continue
                     if len(log_command) != 2:
                         print("Input date is not valid.")
                         continue
